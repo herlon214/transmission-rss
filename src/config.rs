22fd4b55194d3222cd2ac5abad8a2494d9925296
Robin Appelman <robin@icewind.nl>
2022-07-18 23:07:29 +0200

    allow loading secrets from separate files

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use std::fs::read_to_string;
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Config {
@@ -13,10 +14,44 @@ pub struct Persistence {
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(try_from = "RawTransmission")]
 pub struct Transmission {
     pub url: String,
     pub username: String,
     pub password: String,
+}
+
+impl TryFrom<RawTransmission> for Transmission {
+    type Error = std::io::Error;
+
+    fn try_from(value: RawTransmission) -> Result<Self, Self::Error> {
+        let password = match value.password {
+            TransmissionPassword::Raw { password } => password,
+            TransmissionPassword::File { password_file } => {
+                read_to_string(password_file)?.trim().to_string()
+            }
+        };
+        Ok(Transmission {
+            url: value.url,
+            username: value.username,
+            password,
+        })
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+pub struct RawTransmission {
+    pub url: String,
+    pub username: String,
+    #[serde(flatten)]
+    pub password: TransmissionPassword,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+#[serde(untagged)]
+pub enum TransmissionPassword {
+    Raw { password: String },
+    File { password_file: String },
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -28,11 +63,43 @@ pub struct RssList {
 }
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Notification {
-    pub telegram: Option<TelegramNotification>
+    pub telegram: Option<TelegramNotification>,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(try_from = "RawTelegramNotification")]
 pub struct TelegramNotification {
     pub bot_token: String,
     pub chat_id: i64,
+}
+
+impl TryFrom<RawTelegramNotification> for TelegramNotification {
+    type Error = std::io::Error;
+
+    fn try_from(value: RawTelegramNotification) -> Result<Self, Self::Error> {
+        let bot_token = match value.bot_token {
+            TelegramToken::Raw { bot_token } => bot_token,
+            TelegramToken::File { bot_token_file } => {
+                read_to_string(bot_token_file)?.trim().to_string()
+            }
+        };
+        Ok(TelegramNotification {
+            bot_token,
+            chat_id: value.chat_id,
+        })
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct RawTelegramNotification {
+    #[serde(flatten)]
+    pub bot_token: TelegramToken,
+    pub chat_id: i64,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+#[serde(untagged)]
+pub enum TelegramToken {
+    Raw { bot_token: String },
+    File { bot_token_file: String },
 }
